3e8929888726a3880c6162416828634193d2a0eb
thisistrivial <g.lee13770334@gmail.com>
2020-10-25 02:29:18 -0400

    Added Pauli transformations

--- src/quant.py
+++ src/quant.py
@@ -3,12 +3,40 @@ from cplx import *
 
 #Hadamard
 def H(cm : cmat):
-  hrows = 2
-  hcols = 2
-  hscalar = cnum(1 / 2 ** 0.5, 0)
-  hentries = {(0, 0):cnum(1, 0),
-              (1, 0):cnum(1, 0),
-              (0, 1):cnum(1, 0),
-              (1, 1):cnum(-1, 0)}
-  h = cmat(hrows, hcols, hentries).smult(hscalar)
-  return cmatmult(cm, h)
+  rows = 2
+  cols = 2
+  scalar = cnum(1 / 2 ** 0.5, 0)
+  entries = {(0, 0):cnum(1, 0),
+             (1, 0):cnum(1, 0),
+             (0, 1):cnum(1, 0),
+             (1, 1):cnum(-1, 0)}
+  m = cmat(rows, cols, entries).smult(scalar)
+  return cmatmult(cm, m)
+
+#Pauli-X
+def X(cm : cmat):
+  rows = 2
+  cols = 2
+  entries = {(0, 1):cnum(1, 0),
+             (1, 0):cnum(1, 0)}
+  m = cmat(rows, cols, entries)
+  return cmatmult(cm, m)
+
+#Pauli-Y
+def Y(cm : cmat):
+  rows = 2
+  cols = 2
+  entries = {(0, 1):cnum(0, -1),
+             (1, 0):cnum(0, 1)}
+  m = cmat(rows, cols, entries)
+  return cmatmult(cm, m)
+
+#Pauli-Z
+def Z(cm : cmat):
+  rows = 2
+  cols = 2
+  entries = {(0, 0):cnum(1, 0),
+             (1, 1):cnum(-1, 0)}
+  m = cmat(rows, cols, entries)
+  return cmatmult(cm, m)
+
