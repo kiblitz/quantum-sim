e27067e46c7e2a2fb092200b4859d71c8acf17cc
thisistrivial <g.lee13770334@gmail.com>
2020-10-25 13:11:31 -0400

    Added Phase and T gates

--- src/quant.py
+++ src/quant.py
@@ -2,6 +2,8 @@
 from cplx import *
 from typing import Tuple
 
+import math
+
 # Zero qubit
 def zero() -> cmat:
   rows = 2
@@ -60,6 +62,24 @@ def Z(cm : cmat) -> cmat:
   cols = 2
   entries = {(0, 0):cnum(1),
              (1, 1):cnum(-1)}
+  m = cmat(rows, cols, entries)
+  return cmatmult(m, cm)
+
+# Phase (S)
+def S(cm : cmat) -> cmat:
+  rows = 2
+  cols = 2
+  entries = {(0, 0):cnum(1),
+             (1, 1):cnum(0, 1)}
+  m = cmat(rows, cols, entries)
+  return cmatmult(m, cm)
+
+# pi/8 (T)
+def T(cm : cmat) -> cmat:
+  rows = 2
+  cols = 2
+  entries = {(0, 0):cnum(1),
+             (1, 1):cexp(math.pi/4)}
   m = cmat(rows, cols, entries)
   return cmatmult(m, cm)
 
