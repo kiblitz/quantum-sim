7da14270bd5a795327c7847da01609e28f5a077e
thisistrivial <g.lee13770334@gmail.com>
2020-10-25 01:33:07 -0400

    Added complex matrices

--- src/complex.py
+++ src/complex.py
@@ -4,7 +4,7 @@ class cnum:
     self.real = real
     self.imag = imag
 
-  def string(self):
+  def to_string(self):
     if self.imag < 0:
       op = '-'
     else:
@@ -12,7 +12,7 @@ class cnum:
     return str(self.real) + op + str(abs(self.imag))
 
   def display(self):
-    print(self.string())
+    print(self.to_string())
 
 def conj(c : cnum):
   return cnum(c.real, -c.imag)
@@ -30,4 +30,42 @@ def cadd(c1 : cnum, c2 : cnum):
   c = c2.real
   d = c2.imag
   return cnum(a + c, b + d)
+
+class cmat:
+  def __init__(self, rows : int, cols : int, entries : Dict[Tuple[int, int], cnum]):
+    self.rows = rows
+    self.cols = cols
+    self.array = [[cnum(0, 0) for c in range(cols)] for r in range(rows)] 
+    for key, value in entries.items:
+      row = key[0]
+      col = key[1]
+      self.array[row][col] = value
+
+  def display(self):
+    sizes = []
+    for col in zip(self.array):
+      sizes.append(max(col, key=lambda c : len(c.to_string())))
+    for row in self.array:
+      line = '['
+      for i in range(len(self.array[row])):
+        num = row[i].to_string()
+        size = len(num) 
+        tail = sizes[i] - size
+        line += num + ' ' * tail + ','
+      print(line + ']')
+
+def cmatmult(cm1 : cmat, cm2 : cmat):
+  shared = cm1.cols
+  if shared != cm2.rows:
+    raise Exception("Matrix multiplication invalid")
+  rows = cm1.rows
+  cols = cm2.cols
+  entries = {}
+  for r in range(rows):
+    for c in range(cols):
+      total = cnum(0, 0)
+      for e in range(shared):
+        total = cadd(total, cmult(cm1.array[r][e], cm2.array[e][c]))
+      entries[(r, c)] = total
+  return cmat(rows, cols, entries)
 
