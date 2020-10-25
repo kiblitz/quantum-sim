876d0bc10be5b0a539049612ad87be78aaf41698
thisistrivial <g.lee13770334@gmail.com>
2020-10-25 03:47:54 -0400

    Added Controlled Not X

--- src/quant.py
+++ src/quant.py
@@ -7,10 +7,10 @@ def H(cm : cmat) -> cmat:
   rows = 2
   cols = 2
   scalar = cnum(1 / 2 ** 0.5, 0)
-  entries = {(0, 0):cnum(1, 0),
-             (1, 0):cnum(1, 0),
-             (0, 1):cnum(1, 0),
-             (1, 1):cnum(-1, 0)}
+  entries = {(0, 0):cnum(1),
+             (1, 0):cnum(1),
+             (0, 1):cnum(1),
+             (1, 1):cnum(-1)}
   m = cmat(rows, cols, entries).smult(scalar)
   return cmatmult(m, cm)
 
@@ -18,8 +18,8 @@ def H(cm : cmat) -> cmat:
 def X(cm : cmat) -> cmat:
   rows = 2
   cols = 2
-  entries = {(0, 1):cnum(1, 0),
-             (1, 0):cnum(1, 0)}
+  entries = {(0, 1):cnum(1),
+             (1, 0):cnum(1)}
   m = cmat(rows, cols, entries)
   return cmatmult(m, cm)
 
@@ -36,7 +36,20 @@ def Y(cm : cmat):
 def Z(cm : cmat):
   rows = 2
   cols = 2
-  entries = {(0, 0):cnum(1, 0),
-             (1, 1):cnum(-1, 0)}
+  entries = {(0, 0):cnum(1),
+             (1, 1):cnum(-1)}
   m = cmat(rows, cols, entries)
   return cmatmult(m, cm)
+
+#Controlled Not X
+def CX(cm1 : cmat, cm2 : cmat):
+  cm3 = kronecker(cm1, cm2)
+  rows = 4
+  cols = 4
+  entries = {(0, 0):cnum(1),
+             (1, 1):cnum(1),
+             (2, 3):cnum(1),
+             (3, 2):cnum(1)}
+  return cmatmult(m, cm3)
+
+
