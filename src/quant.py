3ff65025fa88ce0e21dd6316c721f36c27eb32cb
thisistrivial <g.lee13770334@gmail.com>
2020-10-25 03:53:57 -0400

    Fixed syntax errors

--- src/quant.py
+++ src/quant.py
@@ -32,7 +32,7 @@ def X(cm : cmat) -> cmat:
   return cmatmult(m, cm)
 
 #Pauli-Y
-def Y(cm : cmat):
+def Y(cm : cmat) -> cmat:
   rows = 2
   cols = 2
   entries = {(0, 1):cnum(0, -1),
@@ -41,7 +41,7 @@ def Y(cm : cmat):
   return cmatmult(m, cm)
 
 #Pauli-Z
-def Z(cm : cmat):
+def Z(cm : cmat) -> cmat:
   rows = 2
   cols = 2
   entries = {(0, 0):cnum(1),
@@ -50,7 +50,7 @@ def Z(cm : cmat):
   return cmatmult(m, cm)
 
 #Controlled Not X
-def CX(cm1 : cmat, cm2 : cmat):
+def CX(cm1 : cmat, cm2 : cmat) -> cmat:
   cm3 = kronecker(cm1, cm2)
   rows = 4
   cols = 4
@@ -58,6 +58,7 @@ def CX(cm1 : cmat, cm2 : cmat):
              (1, 1):cnum(1),
              (2, 3):cnum(1),
              (3, 2):cnum(1)}
+  m = cmat(rows, cols, entries)
   return cmatmult(m, cm3)
 
 
