6331925649ff6c8a31f3de580f2d7a1ff26256ff
thisistrivial <g.lee13770334@gmail.com>
2020-10-25 03:17:21 -0400

    Fixed gate operations

--- src/quant.py
+++ src/quant.py
@@ -1,8 +1,9 @@
 
 from cplx import *
+from typing import Tuple
 
 #Hadamard
-def H(cm : cmat):
+def H(cm : cmat) -> cmat:
   rows = 2
   cols = 2
   scalar = cnum(1 / 2 ** 0.5, 0)
@@ -11,16 +12,16 @@ def H(cm : cmat):
              (0, 1):cnum(1, 0),
              (1, 1):cnum(-1, 0)}
   m = cmat(rows, cols, entries).smult(scalar)
-  return cmatmult(cm, m)
+  return cmatmult(m, cm)
 
 #Pauli-X
-def X(cm : cmat):
+def X(cm : cmat) -> cmat:
   rows = 2
   cols = 2
   entries = {(0, 1):cnum(1, 0),
              (1, 0):cnum(1, 0)}
   m = cmat(rows, cols, entries)
-  return cmatmult(cm, m)
+  return cmatmult(m, cm)
 
 #Pauli-Y
 def Y(cm : cmat):
@@ -29,7 +30,7 @@ def Y(cm : cmat):
   entries = {(0, 1):cnum(0, -1),
              (1, 0):cnum(0, 1)}
   m = cmat(rows, cols, entries)
-  return cmatmult(cm, m)
+  return cmatmult(m, cm)
 
 #Pauli-Z
 def Z(cm : cmat):
@@ -38,5 +39,4 @@ def Z(cm : cmat):
   entries = {(0, 0):cnum(1, 0),
              (1, 1):cnum(-1, 0)}
   m = cmat(rows, cols, entries)
-  return cmatmult(cm, m)
-
+  return cmatmult(m, cm)
