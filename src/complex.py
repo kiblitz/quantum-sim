a20b4bc79cf236a892fd4b869a0f6c51cd3742d0
thisistrivial <g.lee13770334@gmail.com>
2020-10-25 01:57:59 -0400

    Fixed errors in complex

--- src/complex.py
+++ src/complex.py
@@ -1,30 +1,32 @@
 
+from typing import AnyStr, Dict, Tuple
+
 class cnum:
   def __init__(self, real : float, imag : float = 0):
     self.real = real
     self.imag = imag
 
-  def to_string(self):
+  def to_string(self) -> AnyStr:
     if self.imag < 0:
       op = '-'
     else:
       op = '+'
-    return str(self.real) + op + str(abs(self.imag))
+    return str(self.real) + op + str(abs(self.imag)) + 'i'
 
-  def display(self):
+  def display(self) -> None:
     print(self.to_string())
 
-def conj(c : cnum):
+def conj(c : cnum) -> cnum:
   return cnum(c.real, -c.imag)
 
-def cmult(c1 : cnum, c2 : cnum):
+def cmult(c1 : cnum, c2 : cnum) -> cnum:
   a = c1.real
   b = c1.imag
   c = c2.real
   d = c2.imag
   return cnum(a * c - b * d, b * c + a * d)
 
-def cadd(c1 : cnum, c2 : cnum):
+def cadd(c1 : cnum, c2 : cnum) -> cnum:
   a = c1.real
   b = c1.imag
   c = c2.real
@@ -36,25 +38,25 @@ class cmat:
     self.rows = rows
     self.cols = cols
     self.array = [[cnum(0, 0) for c in range(cols)] for r in range(rows)] 
-    for key, value in entries.items:
+    for key, value in entries.items():
       row = key[0]
       col = key[1]
       self.array[row][col] = value
 
-  def display(self):
+  def display(self) -> None:
     sizes = []
-    for col in zip(self.array):
-      sizes.append(max(col, key=lambda c : len(c.to_string())))
+    for col in [*zip(*self.array)]:
+      sizes.append(len(max(col, key=lambda c : len(c.to_string())).to_string()))
     for row in self.array:
       line = '['
-      for i in range(len(self.array[row])):
+      for i in range(len(row)):
         num = row[i].to_string()
         size = len(num) 
         tail = sizes[i] - size
         line += num + ' ' * tail + ','
-      print(line + ']')
+      print(line[:-1] + ']')
 
-def cmatmult(cm1 : cmat, cm2 : cmat):
+def cmatmult(cm1 : cmat, cm2 : cmat) -> cmat:
   shared = cm1.cols
   if shared != cm2.rows:
     raise Exception("Matrix multiplication invalid")
