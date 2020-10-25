11900ef4b44281d630e11b5a0d44e15a85225476
thisistrivial <g.lee13770334@gmail.com>
2020-10-25 14:22:21 -0400

    Added print ket function

--- src/qubit.py
+++ src/qubit.py
@@ -1,6 +1,8 @@
 
 from cplx import *
 
+import math
+
 # Zero qubit
 def zero() -> cmat:
   rows = 2
@@ -15,3 +17,21 @@ def one() -> cmat:
   entries = {(1, 0):cnum(1)}
   return cmat(rows, cols, entries)
 
+# Print Dirac notation of qubits (Bra-ket)
+def ket(cm : cmat, rem0 : bool = True, condensed : bool = False) -> None:
+  if cm.cols != 1:
+    raise Exception("Qubit complex matrix must have 1 col")
+  lg = math.log(cm.rows, 2)
+  qubits = (int)(lg)
+  if qubits != lg:
+    raise Exception("Qubit must contain 2^n rows")
+  expression = ''
+  for i in range(cm.rows):
+    state = bin(i)[2:] 
+    state = '0' * (qubits - len(state)) + state
+    val = cm.array[i][0].to_string(rem0, condensed)
+    space = ' ' * (not condensed)
+    expression += '%s(%s)|%s)%s+'  % (space, val, state, space)
+  if not condensed:
+    expression = expression[1:-1]
+  print(expression[:-1])
